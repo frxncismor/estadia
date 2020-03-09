dd3819be9467a4b24a536b8f4a03434d0d7129cc
Francisco J. Moreno <franciscomoreno@contralor.mx>
2020-03-09 10:26:45 -0600

    about-what content

--- src/app/pages/about-what/about-what.component.ts
+++ src/app/pages/about-what/about-what.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-what',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutWhatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  back2begin(){
+    this.route.navigateByUrl('/');
   }
 
 }
